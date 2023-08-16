117ff8b65bb644f7e71b2b4767e12ed4c272039c
Valentina M <70953225+ScriptyShake@users.noreply.github.com>
2023-08-16 21:12:44 +0200

    Basic character movement with Unity Built-in physics.
    - Removed the custom physics code and added back the built-in Unity physics with RigidBody2D and BoxCollider2D.
    - Used a physics material on the CharacterController RigidBody to remove all friction.
    - Made the Player and Platforms into Prefabs and reorganized the level.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,71 +10,30 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float walkingSpeed;
     [SerializeField] private float jumpSpeed;
     [SerializeField] private float gravity;
-    [Space]
-    [SerializeField] private Transform boxColliderDown;
-    [SerializeField] private Transform boxColliderUp;
-    [SerializeField] private Transform boxColliderLeft;
-    [SerializeField] private Transform boxColliderRight;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
     
-    private bool _inAir = true;
-    private ContactFilter2D _filter2D;
+    private bool _inAir;
     private float _direction;
     private Vector2 _velocity;
-    private Collider2D[] _resultsDown = new Collider2D[1];
-    private Collider2D[] _resultsUp = new Collider2D[1];
-    private Collider2D[] _resultsLeft = new Collider2D[1];
-    private Collider2D[] _resultsRight = new Collider2D[1];
     
     #endregion
     
     
     void Start()
     {
-
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
     
     void Update()
     {
+        _velocity = _rigidbody2D.velocity;
+        
         // Horizontal movement
         _direction = Input.GetAxis("Horizontal");
         _velocity.x = walkingSpeed * _direction;
         
         // Apply gravity
         _velocity.y -= gravity * Time.deltaTime;
-
-        #region Collision
-
-        // Ground/Down Collision Detection
-        if (Physics2D.OverlapBox(boxColliderDown.position, boxColliderDown.localScale, 0, _filter2D, _resultsDown) > 0 && _velocity.y < 0)
-        {
-            _velocity.y = 0;
-            _inAir = false;
-        }
-        else
-        {
-            _inAir = true;
-        }
-        
-        // Up Collision Detection
-        if (Physics2D.OverlapBox(boxColliderUp.position, boxColliderUp.localScale, 0, _filter2D, _resultsUp) > 0 && _velocity.y > 0)
-        {
-            _velocity.y = 0;
-        }
-        
-        // Left Collision Detection
-        if (Physics2D.OverlapBox(boxColliderLeft.position, boxColliderLeft.localScale, 90, _filter2D, _resultsLeft) > 0 && _velocity.x < 0)
-        {
-            _velocity.x = 0;
-        }
-        
-        // Right Collision Detection
-        if (Physics2D.OverlapBox(boxColliderRight.position, boxColliderRight.localScale, 90, _filter2D, _resultsRight) > 0 && _velocity.x > 0)
-        {
-            _velocity.x = 0;
-        }
-
-        #endregion
-        
 
         // Jump
         if (Input.GetButtonDown("Jump") && !_inAir)
@@ -83,6 +42,6 @@ public class PlayerController : MonoBehaviour
         }
         
         // Final movement
-        transform.Translate(_velocity * Time.deltaTime);
+        _rigidbody2D.velocity = _velocity;
     }
 }
