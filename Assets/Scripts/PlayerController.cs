87e861ddbe2e9ff4766d2a987452a675efc5a474
Valentina M <70953225+ScriptyShake@users.noreply.github.com>
2023-08-18 21:02:42 +0200

    Added Clamped Fall Speed
    - Gravity gets higher as you fall.
    - There is a maximum gravity you can reach when falling.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+[RequireComponent (typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     #region Variables
@@ -10,8 +12,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float walkingSpeed;
     [SerializeField] private float jumpSpeed;
     [SerializeField] private float gravity;
+    [SerializeField] private float gravityMultiplier;
+    [SerializeField] private float maxFallSpeed;
     [SerializeField] private Rigidbody2D _rigidbody2D;
-    
+
+    private float _gravityTemp;
     private bool _inAir;
     private float _direction;
     private Vector2 _velocity;
@@ -22,8 +27,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _gravityTemp = gravity;
     }
-    
+
     void Update()
     {
         _velocity = _rigidbody2D.velocity;
@@ -35,6 +41,17 @@ public class PlayerController : MonoBehaviour
         // Apply gravity
         _velocity.y -= gravity * Time.deltaTime;
 
+        // Increase gravity when falling
+        if (_rigidbody2D.velocity.y < 0 && _inAir)
+        {
+            _gravityTemp *= gravityMultiplier;
+            _velocity.y = Mathf.Max(_rigidbody2D.velocity.y, -maxFallSpeed);
+        }
+        else if (!_inAir)
+        {
+            _gravityTemp = gravity;
+        }
+        
         // Jump
         if (Input.GetButtonDown("Jump") && !_inAir)
         {
