d4790c805fff0a10f82d8e72a98d7f19f1c7979a
Valentina M <70953225+ScriptyShake@users.noreply.github.com>
2023-08-21 17:02:16 +0200

    Added ground check.
    - I realized I didn't add the ground check in the previous versions when remaking the physics with Unity Physics.
    - I also has started implementing variable gravity when jumping to make the falling speed faster but it broke the game so I commented it.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     #region Variables
@@ -17,13 +17,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbody2D;
 
     private float _gravityTemp;
-    private bool _inAir;
+    [SerializeField] private bool _inAir;
     private float _direction;
     private Vector2 _velocity;
-    
+
     #endregion
-    
-    
+
+
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -33,32 +33,56 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         _velocity = _rigidbody2D.velocity;
-        
+
         // Horizontal movement
         _direction = Input.GetAxis("Horizontal");
         _velocity.x = walkingSpeed * _direction;
-        
-        // Apply gravity
-        _velocity.y -= gravity * Time.deltaTime;
 
-        // Increase gravity when falling
+        // Apply gravity
+        _velocity.y -= _gravityTemp * Time.deltaTime;
+
+        /*// Increase gravity when falling
         if (_rigidbody2D.velocity.y < 0 && _inAir)
         {
-            _gravityTemp *= gravityMultiplier;
+            Debug.Log("GravityTemp: " + _gravityTemp);
+            Debug.Log("velocity.y " + _velocity.y);
+            //_gravityTemp *= 1 + (gravityMultiplier * Time.deltaTime);
+            // version of this that works with negative gravity multiplier
+
+            _gravityTemp = 200;
+
             _velocity.y = Mathf.Max(_rigidbody2D.velocity.y, -maxFallSpeed);
         }
         else if (!_inAir)
         {
             _gravityTemp = gravity;
-        }
-        
+        }*/
+
         // Jump
         if (Input.GetButtonDown("Jump") && !_inAir)
         {
             _velocity.y = jumpSpeed;
         }
-        
+
         // Final movement
         _rigidbody2D.velocity = _velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            _inAir = false;
+            Debug.Log("OnCollisionEnter2D: Landed on platform");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            _inAir = true;
+            Debug.Log("OOnCollisionExit2D: Left the platform");
+        }
     }
 }
